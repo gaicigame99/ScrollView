ab9a4edc25d4e3dde3501fbf2d2ea03c761aac1d
Lancelot <gaicigame99@163.com>
2016-04-06 15:51:55 +0800

    实现scrollView的xib

---
+++ scrollxib搭建/0406scrollView－1/0406scrollView－1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0406scrollView－1
+//
+//  Created by Lancelot on 16/4/6.
+//  Copyright © 2016年 tedu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scrollxib搭建/0406scrollView－1/0406scrollView－1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  0406scrollView－1
+//
+//  Created by Lancelot on 16/4/6.
+//  Copyright © 2016年 tedu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
